bf6991793cf532a2b3a417dd83a24e119e4c38c4
Peter Boling <peter.boling@gmail.com>
2022-05-02 04:43:30 +0700

    🔥 Remove extra load path mod
    Signed-off-by: Peter Boling <peter.boling@gmail.com>

--- lib/rubocop/ruby2_0.rb
+++ lib/rubocop/ruby2_0.rb
@@ -1,8 +1,5 @@
 # frozen_string_literal: true
 
-# NOTE: Using __FILE__ because require_relative was added in Ruby 2.1
-lib = File.expand_path('../lib', __FILE__)
-$LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
 require "rubocop/ruby2_0/version"
 
 module Rubocop
